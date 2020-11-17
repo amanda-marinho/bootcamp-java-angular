24ebd0d82ea3436c4ecd6975cc33ac9a8bde9e9e
Amanda Marinho <amanda.marinho@usp.br>
2020-11-16 23:38:08 -0300

    Introdução a programação com JS

---
+++ intro-programacao-js/distancia.js
@@ -0,0 +1,12 @@
+// Entrada
+// O arquivo de entrada contém um número inteiro K que representa a quantidade de quilômetro que que a moto Y deve estar da moto X.
+
+// Saída
+// Imprima o tempo necessário para a moto Y ficar com a quantidade K de quilômetro da moto X, seguido da mensagem " minutos".
+
+
+let quilometros = parseInt(gets());
+let minutos = quilometros / 0.5; // Digite aqui o calculo dos minutos
+console.log(minutos + " minutos");
+
+
